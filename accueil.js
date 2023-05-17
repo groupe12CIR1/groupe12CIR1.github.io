d25619856dfc2c05fe81a9c6cfc520313f3f6f93
KannedWA <87776426+W-A-W-A@users.noreply.github.com>
2023-05-17 15:04:22 +0200

    Implémentation de l'horloge dans la barre de menu

--- accueil.js
+++ accueil.js
@@ -1,3 +1,20 @@
+setInterval(showTime, 1000);
+function showTime() {
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+  
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+  
+    let currentTime = hour + ":" 
+            + min + ":" + sec;
+  
+    document.getElementById("clock")
+            .innerHTML = currentTime;
+}
 
 
 function members_click(){
@@ -7,3 +24,5 @@ function members_click(){
         alert("ils sont pas si moches quand même :(");
     }
 }
+
+showTime();
