d28792763aab02f00866e03de55e95826a78b044
Clement Robin <robinclem@free.fr>
2023-05-17 15:29:25 +0200

    load_update1

--- offre_demploi.js
+++ offre_demploi.js
@@ -1,20 +1,18 @@
 function main(){
     load();
-    
+    timePage();
 }
 main();
 
 function load(){
-    let load = document.getElementById('body');
+    let load = document.getElementById('load');
 
     // Créez un élément <img> pour afficher le GIF
     const gif = document.createElement('img');
-    gif.src="Images_H/loader.gif";
+    gif.src="Images_H/loading.gif";
     gif.style.position = 'fixed';
-    gif.style.top = '0';
-    gif.style.left = '0';
-    gif.style.width = '100%';
-    gif.style.opacity = '0.8';
+    gif.style.top = '10%';
+    gif.style.left = '32%';
 
     // Insérez le GIF dans l'élément spécifié
     load.appendChild(gif);
@@ -25,4 +23,12 @@ function load(){
     
     }, 4000);
     
+}
+function timePage(){
+    let elem = document.getElementById("body");
+    elem.style.display = 'block';
+    setTimeout(() => {
+        elem.style.display = 'none';
+        
+    }, 4000);
 }
