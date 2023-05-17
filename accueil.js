aa30d7f4eef209b80a491a0eea8cb8c01dfa269a
KannedWA <87776426+W-A-W-A@users.noreply.github.com>
2023-05-17 15:30:10 +0200

    Travail en cours sur le chrono

--- accueil.js
+++ accueil.js
@@ -1,5 +1,10 @@
 setInterval(showTime, 1000);
+var chrono_sec = 0;
+var chrono_min = 0;
+var chrono_hour = 0;
+
 function showTime() {
+    // met à jour l'horloge
     let time = new Date();
     let hour = time.getHours();
     let min = time.getMinutes();
@@ -9,11 +14,35 @@ function showTime() {
     min = min < 10 ? "0" + min : min;
     sec = sec < 10 ? "0" + sec : sec;
   
-    let currentTime = hour + ":" 
-            + min + ":" + sec;
-  
-    document.getElementById("clock")
-            .innerHTML = currentTime;
+    let currentTime = hour + ":" + min + ":" + sec;
+
+    // met à jour le chrono
+    chrono_sec++;
+    if (chrono_sec > 59){
+        chrono_min++;
+        chrono_min = chrono_min < 10 ? "0" + chrono_min : chrono_min;
+        chrono_sec = "0";
+    }
+    if (chrono_min > 59){
+        chrono_hour++;
+        chrono_hour = chrono_hour < 10 ? "0" + chrono_hour : chrono_hour;
+        chrono_min = "0";
+    }
+
+
+
+    currentChrono = chrono_sec + " ";
+    if (chrono_min != 0){
+        currentChrono = chrono_min + ":" + currentChrono;
+    }
+    if (chrono_hour != 0){
+        currentChrono = chrono_hour + ":" + currentChrono;
+    }
+
+    currentTime += "\n" + currentChrono;
+
+    // envoie tout au HTML
+    document.getElementById("clock").innerHTML = currentTime;
 }
 
 
