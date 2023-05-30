115b8d1e26ecbcdbc9b29be6bd1f8f22a0b4ef43
dansunavioon <damrani801@gmail.com>
2023-05-30 19:09:56 +0200

    Page projet finie

--- Projets.js
+++ Projets.js
@@ -7,3 +7,18 @@ monterHaut.addEventListener("click", function() {
     });
 });
 
+//Pour alterner deux images, nous allons changer leur attribut au clic :
+var projectImages = document.querySelectorAll('.projectImage');
+
+projectImages.forEach(function(image) {
+
+    image.addEventListener('click', function() {
+      var currentImageSrc = this.getAttribute('src'); // Récupère l'attribut "src" de l'image actuelle
+
+      var alternateImageSrc = this.getAttribute('data-alternate-src'); // Récupère l'attribut "data-alternate-src" de l'image pour l'autre image
+
+      // Échangez les valeurs des attributs "src" et "data-alternate-src"
+      this.setAttribute('src', alternateImageSrc);
+      this.setAttribute('data-alternate-src', currentImageSrc);
+    });
+});
